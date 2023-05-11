103a9e37a0ae3a3bee086622a5588796be4dcccb
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2023-05-11 20:54:33 +0900

    Initial commit

---
+++ lib/quiz.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/question.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/result_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = "start-screen";
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = "question-screen";
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = "result-screen";
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == "question-screen") {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == "result-screen") {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 100, 219, 143),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          Text("Learn Flutter the fun way!",
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+              )),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 219, 115, 7),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: Text("Start Quiz.",
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
